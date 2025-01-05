170c504e551da706b7b201c626cea37ee90227be
AhsanKhan <ahsankhan.ubit>
2025-01-05 22:59:59 +0500

    Hero Addition on Nextjs

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { HeroHighlight } from './components/Hero'
+
+
+const page = () => {
+  return (
+    <div><HeroHighlight className='text-2xl bg-black text-purple-400' containerClassName=''>Hello I am Full Stack Developer</HeroHighlight></div>
+  )
+}
+
+export default page
